427489c26a494601398aef739137d388423ec06d
Ashfaq Sarker Abid <abidsarker213@gmail.com>
2025-06-10 15:30:06 +0600

    font set

--- src/pages/Shared/tailwind.config.js
+++ src/pages/Shared/tailwind.config.js
@@ -1,4 +1,4 @@
-// tailwind.config.js
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   plugins: [require("daisyui")],
@@ -8,14 +8,14 @@ module.exports = {
         light: {
           primary: "#FFD66B",
           base100: "#ffffff",
-          "base-100": "#ffffff",
+        
         },
       },
       {
         dark: {
           primary: "#0D9488",
           base100: "#1E293B",
-          "base-100": "#1E293B",
+          
         },
       },
     ],
